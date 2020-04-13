6a820131a40dcfa871615fa3b5fe21af6cc8fdf8
Alex Luna <14889546+LeTarrask@users.noreply.github.com>
2020-04-13 11:17:19 +0100

    Created form to receive info from user

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Length, ValidationError
+
+class PlaylistForm(FlaskForm):
+    festivalname = StringField('Festival Name', validators=[DataRequired()])
+    festivalyear = StringField('Festival Year', validators=[DataRequired()])
+    bands = TextAreaField('Bands', validators=[DataRequired(), Length(min=1, max=1000)])
+    submit = SubmitField('Generate Playlist')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template
 from app import app
+from app.forms import PlaylistForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html", title="Remember that night")
+    form = PlaylistForm()
+    return render_template("index.html", title="Remember that night", form = form)
