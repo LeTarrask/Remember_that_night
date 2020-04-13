ff627c8abea57b9df695fc5a948587ef4e56ac90
Alex Luna <14889546+LeTarrask@users.noreply.github.com>
2020-04-13 20:03:24 +0100

    WIP passing data to playlist page

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,20 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import PlaylistForm
+from spotifier.festivalprocessor import FestivalProcessor
 
 @app.route('/', methods=['GET', 'POST'])
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     form = PlaylistForm()
     if form.validate_on_submit():
-        flash('Playlist requested for festival {}, bands={}'.format(
-            form.festivalname.data, form.bands.data))
-        return redirect('/playlist')
+        data_load = [form.festivalname.data, form.festivalyear.data, form.bands.data.splitlines()]
+        #TODO: how to pass data load to the following page????
+        return redirect(url_for("playlist"))
     return render_template("index.html", title="Remember that night", form = form)
 
 @app.route("/playlist")
 def playlist():
+    processor = FestivalProcessor(data_load)
+    playlist = processor.playlist_storage
     return render_template("playlist.html", title="Your Playlist")
